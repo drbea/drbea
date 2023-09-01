0ad198d3e139f4c0411bcaa06e24866ed455921a
drbea <beavoguimaximeakoi@gmail.com>
2023-09-02 01:20:08 +0200

    reoganisation de l'arborescence des fichiers

--- odc/day2/config.js
+++
@@ -1,8 +0,0 @@
-
-
-// Variables
-let tape_mot = false; // Variable booléenne pour indiquer si l'utilisateur a entré un mot.
-let motApplication = "hello"; // Mot à deviner.
-let mot = ""; // Mot entré par l'utilisateur.
-let listeMot = ["Cachalot", "Petunia", "Serviette"]; // Liste des mots à deviner.
-let listePhrases = ["hello world", "Pas de panique", "Merci pour le poisson"]; // Liste des phrases à deviner.
