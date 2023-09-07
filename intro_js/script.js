da24b65840bd25ab16ea1a6b5b393530a923ca2b
drbea <beavoguimaximeakoi@gmail.com>
2023-09-07 17:11:21 +0200

    plusieurs modifications apportees

---
+++ intro_js/script.js
@@ -0,0 +1,13 @@
+
+num = "tel"   
+person = {
+    age: 30,
+    firstname: "john",
+    lastname: "doe",
+    note: [12, 14, 15],
+    job: {
+        name: "informaticien",
+        hour: 35
+    },
+    [num]: 67464 // inclusion d'une variable comme cle
+}
